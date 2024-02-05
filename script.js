e258b1ace85a568814ce3e2d22739fb91cc4a6de
David PC <davidpc4335@gmail.com>
2024-02-05 00:33:35 -0500

    Added fullscreen button and improved resume  resume embed

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+function requestFullScreen(element) {
+  // Supports most browsers and their versions.
+  var requestMethod =
+    element.requestFullScreen ||
+    element.webkitRequestFullScreen ||
+    element.mozRequestFullScreen ||
+    element.msRequestFullscreen;
 
+  if (requestMethod) {
+    // Native full screen.
+    requestMethod.call(element);
+  } else if (typeof window.ActiveXObject !== "undefined") {
+    // Older IE.
+    var wscript = new ActiveXObject("WScript.Shell");
+    if (wscript !== null) {
+      wscript.SendKeys("{F11}");
+    }
+  }
+}
+
+function makeFullScreen() {
+  document.querySelector(".frame__fullscreen").classList.toggle("fullscreen");
+  var elem = document.body;
+  requestFullScreen(elem);
+}
