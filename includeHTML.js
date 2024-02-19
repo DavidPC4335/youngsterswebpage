ded6c484a2eb29f9f7a592371e12de2543040cc6
David PC <davidpc4335@gmail.com>
2024-02-18 23:28:18 -0500

    added trial animation to home page

--- includeHTML.js
+++ includeHTML.js
@@ -26,15 +26,23 @@ function includeHTML() {
       xhttp.send();
       /* Exit the function: */
 
-      const menu = document.querySelector("#mobile-menu");
-      const menuLinks = document.querySelector(".navbar__menu");
-
-      menu.addEventListener("click", function () {
-        menu.classList.toggle("is-active");
-        menuLinks.classList.toggle("active");
-      });
-
+      initPage();
       return;
     }
   }
+}
+
+function initPage() {
+  const menu = document.querySelector("#mobile-menu");
+  const menuLinks = document.querySelector(".navbar__menu");
+
+  menu.addEventListener("click", function () {
+    menu.classList.toggle("is-active");
+    menuLinks.classList.toggle("active");
+  });
+
+  document.getElementById("#background").onload = function () {
+    this.contentWindow.scrollTo(0, 400);
+  };
+  console.log("initPage");
 }
